6d8e32b52ffe933f2cce14779d1028f10e380701
Yixin Nie <ynie@fb.com>
2020-06-30 10:19:40 -0400

    Push code for training.

---
+++ src/flint/data_utils/batchbuilder.py
@@ -0,0 +1,66 @@
+import torch
+from typing import Dict, Type
+
+from flint.data_utils.fields import FlintField, RawFlintField
+
+
+class BaseBatchBuilder(object):
+    def __init__(self, batching_schema: Dict[str, FlintField]) -> None:
+        super().__init__()
+        self.batching_schema: Dict[str, FlintField] = batching_schema
+
+    def __call__(self, batch):
+        field_names = batch[0].keys()
+        batched_data = dict()
+
+        for field_name in field_names:
+            if field_name not in self.batching_schema:
+                # default is RawFlintField
+                batched_data[field_name] = RawFlintField.batching([item[field_name] for item in batch])
+
+            else:
+                batched_data[field_name] = self.batching_schema[field_name].batching([item[field_name] for item in batch])
+
+        return batched_data
+
+
+def has_tensor(obj) -> bool:
+    """
+    Given a possibly complex data structure,
+    check if it has any torch.Tensors in it.
+    """
+    if isinstance(obj, torch.Tensor):
+        return True
+    elif isinstance(obj, dict):
+        return any(has_tensor(value) for value in obj.values())
+    elif isinstance(obj, (list, tuple)):
+        return any(has_tensor(item) for item in obj)
+    else:
+        return False
+
+
+def move_to_device(obj, cuda_device: int):
+    """
+    Given a structure (possibly) containing Tensors on the CPU,
+    move all the Tensors to the specified GPU (or do nothing, if they should be on the CPU).
+    """
+
+    if cuda_device < 0 or not has_tensor(obj):
+        return obj
+    elif isinstance(obj, torch.Tensor):
+        return obj.cuda(cuda_device)
+    elif isinstance(obj, dict):
+        return {key: move_to_device(value, cuda_device) for key, value in obj.items()}
+    elif isinstance(obj, list):
+        return [move_to_device(item, cuda_device) for item in obj]
+    elif isinstance(obj, tuple) and hasattr(obj, "_fields"):
+        # This is the best way to detect a NamedTuple, it turns out.
+        return obj.__class__(*(move_to_device(item, cuda_device) for item in obj))
+    elif isinstance(obj, tuple):
+        return tuple(move_to_device(item, cuda_device) for item in obj)
+    else:
+        return obj
+
+
+if __name__ == '__main__':
+    print(RawFlintField.batching)
---
+++ src/flint/data_utils/fields.py
@@ -0,0 +1,57 @@
+import torch
+
+
+class FlintField(object):
+    @classmethod
+    def batching(cls, batched_data):
+        raise NotImplemented()
+
+
+class RawFlintField(FlintField):
+    @classmethod
+    def batching(cls, batched_data):
+        return batched_data
+
+
+class LabelFlintField(FlintField):
+    def batching(self, batched_data):
+        return torch.tensor(batched_data)
+
+
+class ArrayIndexFlintField(FlintField):
+    def __init__(self, pad_idx, eos_idx=None, left_pad=False, move_eos_to_beginning=False) -> None:
+        super().__init__()
+        self.pad_idx = pad_idx
+        self.eos_idx = eos_idx
+        self.left_pad = left_pad
+        self.move_eos_to_beginning = move_eos_to_beginning
+
+    def collate_tokens(self, values, pad_idx, eos_idx=None, left_pad=False, move_eos_to_beginning=False):
+        """
+        Convert a list of 1d tensors into a padded 2d tensor.
+        """
+        if not torch.is_tensor(values[0]):
+            values = [torch.tensor(v) for v in values]
+
+        size = max(v.size(0) for v in values)
+        res = values[0].new(len(values), size).fill_(pad_idx)
+
+        def copy_tensor(src, dst):
+            assert dst.numel() == src.numel()
+            if move_eos_to_beginning:
+                assert src[-1] == eos_idx
+                dst[0] = eos_idx
+                dst[1:] = src[:-1]
+            else:
+                dst.copy_(src)
+
+        for i, v in enumerate(values):
+            copy_tensor(v, res[i][size - len(v):] if left_pad else res[i][:len(v)])
+        return res
+
+    def batching(self, batched_data):
+        return self.collate_tokens(batched_data,
+                                   self.pad_idx,
+                                   self.eos_idx,
+                                   self.left_pad,
+                                   self.move_eos_to_beginning)
