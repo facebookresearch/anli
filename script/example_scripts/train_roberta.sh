f573d505a3c3347905ad71454b24b61b5d09d007
Yixin Nie <ynie@fb.com>
2020-07-03 17:30:53 -0400

    Update readme and instructions. Also some examples scripts.

---
+++ script/example_scripts/train_roberta.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export MASTER_PORT=88888
+
+echo $CUDA_VISIBLE_DEVICES
+nvidia-smi
+# End visible GPUs.
+
+# setup conda environment
+conda activate conda_parlai_summer2020
+source setup.sh
+
+which python
+
+python src/nli/training.py \
+  --model_class_name "roberta-large" \
+  -n 1 \
+  -g 8 \
+  -nr 0 \
+  --fp16 \
+  --fp16_opt_level O2 \
+  --max_length 156 \
+  --gradient_accumulation_steps 1 \
+  --per_gpu_train_batch_size 16 \
+  --per_gpu_eval_batch_size 32 \
+  --save_prediction \
+  --train_data \
+snli_train:none,mnli_train:none,fever_train:none,anli_r1_train:none,anli_r2_train:none,anli_r3_train:none \
+  --train_weights \
+1,1,1,10,20,10 \
+  --eval_data \
+snli_dev:none,mnli_m_dev:none,mnli_mm_dev:none,anli_r1_dev:none,anli_r2_dev:none,anli_r3_dev:none \
+  --eval_frequency 2000 \
+  --experiment_name "roberta-large|snli+mnli+fnli+r1*10+r2*20+r3*10|nli"
---
+++ script/example_scripts/train_roberta_small.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export MASTER_PORT=88888
+
+echo $CUDA_VISIBLE_DEVICES
+nvidia-smi
+# End visible GPUs.
+
+# setup conda environment
+conda activate conda_parlai_summer2020
+source setup.sh
+
+which python
+
+python src/nli/training.py \
+  --model_class_name "roberta-base" \
+  -n 1 \
+  -g 1 \
+  --single_gpu \
+  -nr 0 \
+  --fp16 \
+  --fp16_opt_level O2 \
+  --max_length 156 \
+  --gradient_accumulation_steps 4 \
+  --per_gpu_train_batch_size 4 \
+  --per_gpu_eval_batch_size 16 \
+  --save_prediction \
+  --train_data \
+anli_r1_train:none,anli_r2_train:none,anli_r3_train:none \
+  --train_weights \
+1,1,1,10,20,10 \
+  --eval_data \
+anli_r1_dev:none,anli_r2_dev:none,anli_r3_dev:none \
+  --eval_frequency 2000 \
+  --experiment_name "roberta-base|snli+mnli+fnli+r1*10+r2*20+r3*10|nli"
