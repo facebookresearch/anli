daadf77d626d1e986a82feec7250aa791e7672c7
Yixin Nie <ynie@fb.com>
2020-07-06 17:16:18 -0400

    Copyright header.

--- script/example_scripts/train_roberta_small.sh
+++ script/example_scripts/train_roberta_small.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under Creative Commons-Non Commercial 4.0 found in the
+# LICENSE file in the root directory of this source tree.
 
 export MASTER_PORT=88888
 
