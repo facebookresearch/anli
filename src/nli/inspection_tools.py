6557047911c91357d27aee0aebe51042de463228
Yixin Nie <nyixin318@gmail.com>
2020-08-31 11:58:18 -0400

    Add headers and fix evaluation import.

--- src/nli/inspection_tools.py
+++ src/nli/inspection_tools.py
@@ -1,3 +1,9 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under Creative Commons-Non Commercial 4.0 found in the
+# LICENSE file in the root directory of this source tree.
+
+
 import torch
 import logging
 from captum.attr import LayerIntegratedGradients
